f948d6430213c29a788aec173abde1a03538eef4
Moty <36139920+projects22@users.noreply.github.com>
2021-11-20 14:53:17 +0000

    Add files via upload

---
+++ VS2010 code/serial counter USB/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.cmb1 = New System.Windows.Forms.ComboBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.sp1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.text1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.text2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmb1
+        '
+        Me.cmb1.FormattingEnabled = True
+        Me.cmb1.Location = New System.Drawing.Point(6, 19)
+        Me.cmb1.Name = "cmb1"
+        Me.cmb1.Size = New System.Drawing.Size(103, 21)
+        Me.cmb1.TabIndex = 70
+        Me.cmb1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 58
+        Me.Label1.Text = "Select"
+        '
+        'text1
+        '
+        Me.text1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.text1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.text1.ForeColor = System.Drawing.Color.Lime
+        Me.text1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.text1.Location = New System.Drawing.Point(42, 12)
+        Me.text1.Name = "text1"
+        Me.text1.ReadOnly = True
+        Me.text1.Size = New System.Drawing.Size(154, 38)
+        Me.text1.TabIndex = 63
+        Me.text1.TabStop = False
+        Me.text1.Text = "000"
+        Me.text1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cmb1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 116)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(154, 60)
+        Me.GroupBox1.TabIndex = 64
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Port Number"
+        '
+        'text2
+        '
+        Me.text2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.text2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.text2.ForeColor = System.Drawing.Color.Red
+        Me.text2.Location = New System.Drawing.Point(42, 64)
+        Me.text2.Name = "text2"
+        Me.text2.ReadOnly = True
+        Me.text2.Size = New System.Drawing.Size(154, 38)
+        Me.text2.TabIndex = 65
+        Me.text2.TabStop = False
+        Me.text2.Text = "000"
+        Me.text2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(202, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 25)
+        Me.Label2.TabIndex = 66
+        Me.Label2.Text = "Hz"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(202, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 25)
+        Me.Label3.TabIndex = 67
+        Me.Label3.Text = "uS"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(278, 188)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.text2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.text1)
+        Me.Name = "Form1"
+        Me.Text = "Frequency Counter"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmb1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents sp1 As System.IO.Ports.SerialPort
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents text2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents text1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ VS2010 code/serial counter USB/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        If sp1.IsOpen Then sp1.Close()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        GetPorts()
+    End Sub
+    Sub GetPorts()
+        For Each sp As String In My.Computer.Ports.SerialPortNames ' Show all available COM ports.
+            cmb1.Items.Add(sp)
+        Next
+    End Sub
+    Private Sub cmb1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmb1.SelectedIndexChanged
+        On Error Resume Next
+        sp1.PortName = cmb1.SelectedItem
+        sp1.BaudRate = 1200
+        sp1.DataBits = 8
+        sp1.Open()
+        Label1.Text = "Port is Open"
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim inB(3) As Byte
+        Dim b1, b2, b3 As UInteger
+        Dim freq As ULong
+        Dim period As Double ', freq
+
+        If sp1.BytesToRead > 2 Then
+            sp1.Read(inB, 0, 3)
+            b1 = inB(0)
+            b2 = inB(1)
+            b3 = inB(2)
+            freq = b1 + (b2 * 256) + (b3 * 65536)
+            text1.Text = FormatNumber(freq, 0)
+            period = 1000000 / freq
+            If freq = 0 Then period = 0
+            If period < 10000 Then
+                text2.Text = Format(period, "0.000")
+            Else
+                text2.Text = FormatNumber(period, 0)
+            End If
+        End If
+    End Sub
+End Class
